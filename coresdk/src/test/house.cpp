3f75ced3beb5222be103f1f0b1b12c7f66330b61
WhyPenguins <sean@whypenguins.com>
2023-11-15 21:20:07 +1100

    Simple test of calling C code from Javascript. Run the following in the browser's console to see: start_main = Module.cwrap('start_main', 'number', []) start_main()
    different_render = Module.cwrap('different_render', 'number', [])
    different_render()

--- coresdk/src/test/house.cpp
+++ coresdk/src/test/house.cpp
@@ -3,19 +3,28 @@
 #include "utils.h"
 
 using namespace splashkit_lib;
-int main()
-{
-    open_window("Shapes by ...", 800, 600);
 
-    clear_screen(COLOR_WHITE);
-    fill_ellipse(COLOR_BRIGHT_GREEN, 0, 400, 800, 400);
-    fill_rectangle(COLOR_GRAY, 300, 300, 200, 200);
-    fill_triangle(COLOR_RED, 250, 300, 400, 150, 550, 300);
-    refresh_screen();
+extern "C" {
+	int start_main()
+	{
+		open_window("Shapes by ...", 800, 600);
 
-    delay(5000);
+		clear_screen(COLOR_WHITE);
+		fill_ellipse(COLOR_BRIGHT_GREEN, 0, 400, 800, 400);
+		fill_rectangle(COLOR_GRAY, 300, 300, 200, 200);
+		fill_triangle(COLOR_RED, 250, 300, 400, 150, 550, 300);
+		refresh_screen();
 
-    return 0;
+		return 0;
+	}
+
+	int different_render()
+	{
+		clear_screen(COLOR_WHITE);
+		fill_ellipse(COLOR_BRIGHT_GREEN, 0, 400, 800, 400);
+		fill_rectangle(COLOR_RED, 300, 300, 200, 200);
+		refresh_screen();
+
+		return 0;
+	}
 }
-
-
